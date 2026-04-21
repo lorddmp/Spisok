c7757ea75dfd5689cd14288a5845331ed0fc8369
lorddmp <dpavlov-07@mail.ru>
2026-04-21 22:43:34 +0300

    Finally it's time to do lists

--- main.cpp
+++ main.cpp
@@ -1,61 +1,82 @@
 #include "spisok.h"
 
+#define IF_ERROR_CALLOC(arg)                \
+do                                          \
+{                                           \
+    if (arg == NULL)                        \
+    {                                       \
+        printf("CALLOC ERROR\n");           \
+        return 1;                           \
+    }                                       \
+} while (0)
+
+void Print_Spisok(str_spisok spisok);
+
 int main()
 {
-    int* data = {};
     str_spisok spisok;
     spisok.data = (int*)calloc(SIZE_DATA, sizeof(int));
     spisok.next = (int*)calloc(SIZE_DATA, sizeof(int));
     spisok.prev = (int*)calloc(SIZE_DATA, sizeof(int));
     spisok.free = 1;
-    spisok.head = 1;
+    spisok.head = 0;                                            //индекс первого элемента списка в data
+    spisok.tail = 0;                                            //индекс последнего элемента списка в data
+    spisok.size = 0;                                            //количество элементов в списке (можно использовать как индикатор для tail или
+                                                                //чтобы после индекса больше чем элементов в data элемент ставился в конец)
 
-    if (spisok.data == NULL)
-    {
-        printf("CALLOC ERROR\n");
-        return 1;
-    }
-
-    if (spisok.next == NULL)
-    {
-        printf("CALLOC ERROR");
-        return 1;
-    }
-
-    if (spisok.prev == NULL)
-    {
-        printf("CALLOC ERROR");
-        return 1;
-    }
+    IF_ERROR_CALLOC(spisok.data);
+    IF_ERROR_CALLOC(spisok.next);
+    IF_ERROR_CALLOC(spisok.prev);
 
     for (int i = 1; i < SIZE_DATA - 1; i++)
         spisok.next[i] = i + 1;
 
-    // for (int i = 2; i < SIZE_DATA - 1; i++)
-    //     spisok.prev[i] = -1;
+    for (int i = 2; i < SIZE_DATA - 1; i++)
+        spisok.prev[i] = -1;
 
-    Vstavka(&spisok, 0, 40);
-    Vstavka(&spisok, 1, 20);
-    Vstavka(&spisok, 2, 30);
+    Print_Spisok(spisok);
+    Vstavka(&spisok, 10, 40);
+    Print_Spisok(spisok);
     Vstavka(&spisok, 0, 10);
+    Print_Spisok(spisok);
+    // Vstavka(&spisok, 2, 20);
+    // Print_Spisok(spisok);
+    // Vstavka(&spisok, 2, 30);
+    // Print_Spisok(spisok);
+    // Vstavka(&spisok, 0, 30);
+    // Print_Spisok(spisok);
 
+    free(spisok.data);
+    free(spisok.next);
+    free(spisok.prev);
+
+    return 0;
+}
+
+void Print_Spisok(str_spisok spisok)
+{
     printf("data: ");
     for (int i = 0; i < SIZE_DATA; i++)
-        printf("%d ", spisok.data[i]);
+        printf("%4d", spisok.data[i]);
     printf("\n");
 
     printf("next: ");
     for (int i = 0; i < SIZE_DATA; i++)
-        printf("%d ", spisok.next[i]);
+        printf("%4d", spisok.next[i]);
     printf("\n");
 
-    // printf("prev: ");
-    // for (int i = 0; i < SIZE_DATA; i++)
-    //     printf("%d ", spisok.next[i]);
-    // printf("\n");
+    printf("prev: ");
+    for (int i = 0; i < SIZE_DATA; i++)
+        printf("%4d", spisok.prev[i]);
+    printf("\n");
+
+    printf("free = %d\n", spisok.free);
+    printf("head = %d\n", spisok.head);
+
+    printf("(0) ");
 
     for (int i = spisok.head; i != 0; i = spisok.next[i])
         printf("%d ", spisok.data[i]);
 
-    return 0;
+    printf("\n\n----------------------------------\n\n");
 }
--- spisok.cpp
+++ spisok.cpp
@@ -2,16 +2,41 @@
 #include <stdlib.h>
 #include "spisok.h"
 
+int Find_Index_In_Next(str_spisok* spisok, int* counter, int* index_next);
+
+int Find_Index_In_Next(str_spisok* spisok, int* counter, int* index_next)
+{
+    if (spisok->head == 0)
+        *counter = 0;
+
+    if (*counter > 0)
+    {
+        *index_next = spisok->next[*index_next];            
+        (*counter)--;
+        Find_Index_In_Next(spisok, counter, index_next);
+    }
+    
+    return *index_next;
+}
+
 void Vstavka(str_spisok* spisok, int index, int value)
 {
     int free = spisok->next[spisok->free];
 
+    spisok->data[spisok->free] = value;
+
+    int start_index = 0;                                                        //индекс в next, где лежит индекс первого элемента списка в data
+    int counter = index;
+    int index_next = Find_Index_In_Next(spisok, &counter, &start_index);        //возвращает индекс в next, куда надо записать индекс нового элемента и откуда взять индекс следующего после него
+                                                                                //а индекс следующего элемента - это индекс элемента, ДО которого вставили новый элемент
+    spisok->prev[spisok->free] = spisok->prev[spisok->next[index_next]];
+    spisok->prev[spisok->next[index_next]] = spisok->free;
+
+    spisok->next[spisok->free] = spisok->next[index_next];
+    spisok->next[index_next] = spisok->free;
+
     if (index == 0)
         spisok->head = spisok->free;
-
-    spisok->data[spisok->free] = value;
-    spisok->next[spisok->free] = spisok->next[index];
-    spisok->next[index] = spisok->free;
 
     spisok->free = free;
 }
